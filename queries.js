366cc769f25dcfe928a5c364ffad78e1051708eb
Scott <Scotthall1983@hotmail.com>
2019-10-26 15:06:31 +0100

    Added getUsersByID, createUser and updateUser.

--- queries.js
+++ queries.js
@@ -8,10 +8,54 @@ const pool = new Pool({
 })
 
 const getUsers = (request, response) => {
-  pool.query( 'SELECT * FROM users ORDER BY id ASC', (error,results) => {
-    if (error){
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
       throw error
     }
     response.status(200).json(results.rows)
   })
+}
+
+const getUsersById = (request, response) => {
+  const id = parseINT(request.params.id)
+
+  pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createUser = (request, response) => {
+  const {
+    name,
+    email
+  } = request.body
+
+  pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${result.insertID}`)
+  })
+}
+
+const updateUser = (request, response) => {
+  const id = parseINT(request.params.id)
+  const {
+    name,
+    email
+  } = request.body
+
+  pool.query(
+    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`USER modified with ID: ${id}`)
+    }
+  )
 }
